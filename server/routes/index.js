fccd7abb1e322f9a4eac4a28f4883139b22930d4
David Vezzani <dcvezzani@churchofjesuschrist.org>
2021-01-12 16:25:55 -0700

    adds FILE_BASE_PATH to env

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var fs = require('fs');
 
 // const DATA_FILE = '/Users/dcvezzani/personal-projects/great-in-eight/server/data-template.json'
-const DATA_FILE = '/var/www/vhosts/vezzaniphotography.com/great-in-eight.vezzaniphotography.com/server/data-template.json'
-const DATA_FILE_V2 = '/var/www/vhosts/vezzaniphotography.com/great-in-eight.vezzaniphotography.com/server/data'
+const DATA_FILE = `${process.env.FILE_BASE_PATH}/server/data-template.json`
+const DATA_FILE_V2 = `${process.env.FILE_BASE_PATH}/server/data`
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
