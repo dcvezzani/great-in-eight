e0da24cf0a5349e4da35bb575e28a56fb82933b2
David Vezzani <dcvezzani@churchofjesuschrist.org>
2021-01-13 08:41:40 -0700

    updates about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Great to Eight</h1>
+
+    <p>This application is a tool that can be used to track progress in the "Great in Eight" exercise program.</p>
+    <p>A valid Facebook account will be required to authenticate with this application.</p>
+    <p>Your personal information will not be shared with any third parties or used for anything other than for associating tracked data with your Facebook account.  No stored data should include any sensitive personal identifiable information.</p>
+    <p>You can delete your stored data at anytime by navigating to <a href="https://great-in-eight.vezzaniphotography.com/deleteMyData">https://great-in-eight.vezzaniphotography.com/deleteMyData</a> </p>
   </div>
 </template>
